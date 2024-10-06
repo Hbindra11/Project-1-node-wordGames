7bda44bfca51761370a7d3521a2455f923835de4
Hema Bindra <hbindra11@yahoo.com>
2024-10-06 21:56:09 +0200

    fixed bug in the encrypted output

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,34 +1,34 @@
 
 const arraryAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
-//let userInput = "hello world"
-//userInput = "do pigs fly"
-//userInput = "going on holiday"
-//const userShiftNumber = 10
-
-// Get two numbers from the command line
+// Get two values from the command line
 const args = process.argv.slice(2);
-// Check if the user provided exactly two numbers
+// Check if the user provided exactly two values
 if (args.length !== 2) {
-  console.error('Please provide a phrase to encrypt and the shift number to use');
-  return;
+    console.error('Please provide a phrase to encrypt and the shift number to use');
+    return;
 }
-console.log(args);
-return
 
-let encrypted =""
+let userInput = args[0].toString().trim().toLowerCase();
+let userShiftNumber = parseInt(args[1])
+let encrypted = ""
 let shiftNumber = 0
-for (let i = 0; i < userInput.length + 1; i++)
-{
-    arraryAlphabet.forEach((letter) =>
-    {
-        if (letter === userInput[i])
-        {
-           shiftNumber =  arraryAlphabet.indexOf(letter)+userShiftNumber
-          if (shiftNumber>25 ){shiftNumber = shiftNumber-26}
-            encrypted = encrypted + arraryAlphabet[shiftNumber]
-         shiftNumber = 0
+for (let i = 0; i < userInput.length + 1; i++) {
+    arraryAlphabet.forEach((letter) => {
+        if (letter === userInput[i]) {
+            shiftNumber = arraryAlphabet.indexOf(letter) + userShiftNumber;
+            if (shiftNumber > 25) { shiftNumber = shiftNumber - 26 };
+            encrypted = encrypted + arraryAlphabet[shiftNumber];
+            shiftNumber = 0;
+            return;
         }
-        if (userInput[i] === " "){encrypted = encrypted+" "}
-    })
-}console.log (encrypted)
+
+
+    }); 
+    
+    if (userInput[i] === " ") {
+        encrypted = encrypted.concat(" ")
+    }
+
+}
+console.log("Encrypted output: " + encrypted)
